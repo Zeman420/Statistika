2adaa19856047a5bc1a7c2c39afbea2c40dd7f19
David Čejka <Cejka.David@stud.vassboskovice.cz>
2021-11-23 08:39:07 +0100

    Přidejte soubory projektu.

---
+++ Statistika/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Statistika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey(false);
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+
+                }
+            } while (keyInfo.Key != ConsoleKey.Escape) ;
+        }
+    }
+}
